05282f86ff584b980c91d2372f351c0064d3ba29
Adrian Perez <adrianp1299@gmail.com>
2022-09-05 12:50:00 -0600

    MVP is completed

--- index.js
+++ index.js
@@ -15,8 +15,22 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age) {
+this.name = name;
+this.age = age;
+this.stomach = []
+}
+Person.prototype.eat = function(edible){
+if (this.stomach.length < 10){
+  this.stomach.push(edible);
+}
+}
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
 
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`
 }
 
 
@@ -36,11 +50,19 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+this.model = model;
+this.milesPerGallon = milesPerGallon;
+this.tank = 0;
+this.odometer = 0;
 }
-
-
+Car.prototype.fill = function(gallons){
+  this.tank = this.tank + gallons;
+}
+Car.prototype.drive = function(distance){
+this.odometer = this.odometer + distance;
+this.tank = this.tank - distance / this.milesPerGallon;
+}
 /*
   TASK 3
     - Write a Baby constructor subclassing Person.
@@ -49,18 +71,22 @@ function Car() {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+Person.call(this, name, age);
+this.favoriteToy= favoriteToy;
 }
-
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
+}
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window/Global Binding - it will be the window/console Object
+  2. Implicit Binding when its got a dot itll be that object
+  3. New Binding specific instance of the object that is created by a constructor function
+  4. Explicit binding is overriding constructor objects by calling them explicitly with ne functions, .call and .apply 
 */
 
 ///////// END OF CHALLENGE /////////
